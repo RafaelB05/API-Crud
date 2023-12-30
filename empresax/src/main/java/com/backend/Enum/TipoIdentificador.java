6071fcf14cd38ee74beefca1cd0d0d7b5dd08f79
Rafael Brunini Pereira <bruninirafael@gmail.com>
2023-12-30 10:34:55 -0300

    Adicionando Validação de CPF

--- empresax/src/main/java/com/backend/Enum/TipoIdentificador.java
+++ empresax/src/main/java/com/backend/Enum/TipoIdentificador.java
@@ -15,7 +15,33 @@ public enum TipoIdentificador{
 
         @Override
         public boolean validaIdentificador(String identificador) {
-            return
+            int digito1, digito2,digitoCPF;
+            int d1 = 0, d2 = 0 ,resto = 0;
+            for(int i = 1; i < identificador.length()-1;i++){
+                digitoCPF = Integer.parseInt(identificador.substring(i - 1, i));
+                d1 = d1 +(11-i) * digitoCPF;
+                d2 = d2 +(12-i) * digitoCPF;
+
+            }
+            resto = (d1 % 11);
+            if(resto < 2)
+                digito1 = 0;
+            else
+                digito1 = 11 - resto;
+
+            d2 += 2 * digito1;
+
+            resto = (d2%11);
+
+            if(resto < 2)
+                digito2 = 0;
+            else
+                digito2 = 11 - resto;
+
+            String nDigVerific = identificador.substring(identificador.length() - 2);
+            String nDigResult = String.valueOf(digito1) + String.valueOf(digito2);
+
+            return nDigVerific.equals(nDigResult);
         }
     },
     CNPJ("CNPJ"){
@@ -30,7 +56,7 @@ public enum TipoIdentificador{
 
         @Override
         public boolean validaIdentificador(String identificador) {
-            return false;
+           return  false;
         }
     },
     EU("EU"){
