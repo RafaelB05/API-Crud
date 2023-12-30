7542c08d7c4eb43b6c8ecd28e91440e65740df6d
Rafael Brunini Pereira <bruninirafael@gmail.com>
2023-12-30 18:47:12 -0300

    Modularização em MicroServices

--- emprestimo-pay/payment/src/main/java/com/empresax/payment/Repositories/PaymentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.empresax.payment.Repositories;
-
-import com.empresax.payment.Models.Emprestimo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Emprestimo,Integer> {}
